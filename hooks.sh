8c74e137949b1c71fccbd6c42cf16b778dd7d0ef
Nick <nick@kousu.ca>
2021-04-19 23:37:20 -0400

    Test and fix enough bugs to make this usable.

--- hooks.sh
+++ hooks.sh
@@ -1,31 +1,26 @@
 #!/bin/sh
 
-run-parts() {
+run_parts() {
   # a replacement for run-parts(8)
   # that supports passing arguments
   # (run-parts supports this but demands they are passed individually, which is not compatible with the "$@" special-case for writing wrapper scripts)
   # usage: run-parts DIRECTORY [arg... ]
-  
+
   # TODO: support --verbose; requires getting getopts out I guess.
-  
+
   dir="$1"; shift
-  
-  ls -1 "${dir}" | while read prog; do
-    "${prog}" "$@"
-  done
+  if [ -d "$dir" ]; then
+    ls -1 "${dir}" | sort | while read prog; do
+      if [ -x "${dir}/${prog}" ]; then
+        echo "Running ${dir}/${prog}"
+        "${dir}/${prog}" "$@"
+      fi
+    done
+  fi
 }
 
 operation="$1"; shift
-case "${operation}" in
-  clean_challenge)
-  deploy_challenge)
-  deploy_cert)
-    "${operation}" "$@"
-    ;;
-  *)
-    echo "Unsupported operation '${operation}'" >&2
-    exit 1;
-    ;;
-esac
-
-run-parts "${BASEDIR}"/hooks/"${operation}".d/ "$@"
+(
+  cd "${BASEDIR}"/hooks/ # pretty-print the script that's running by only passing the relevant subpath to run_parts
+  run_parts "${operation}".d "$@" | sed 's/^/ + /'
+)
