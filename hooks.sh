4c0bd2a436b5dbc50ebd28503ecd345db3213cb9
Nick <nick@kousu.ca>
2021-04-20 00:50:20 -0400

    Update hooks.sh

--- hooks.sh
+++ hooks.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 
-run-parts() {
+run_parts() {
   # a replacement for run-parts(8)
   # that supports passing arguments
   # (run-parts supports this but demands they are passed individually, which is not compatible with the "$@" special-case for writing wrapper scripts)
   # usage: run-parts DIRECTORY [arg... ]
-  
+
   # TODO: support --verbose; requires getting getopts out I guess.
-  
+
   dir="$1"; shift
-  
-  ls -1 "${dir}" | while read prog; do
-    "${prog}" "$@"
-  done
+  if [ -d "$dir" ]; then
+    ls -1 "${dir}" 2>/dev/null | while read prog; do
+      if [ -x "$prog" ]; then
+        "${prog}" "$@"
+      fi
+    done
+  fi
 }
 
 operation="$1"; shift
 case "${operation}" in
-  clean_challenge)
-  deploy_challenge)
-  deploy_cert)
+  clean_challenge|deploy_challenge|deploy_cert)
     "${operation}" "$@"
-    ;;
-  *)
-    echo "Unsupported operation '${operation}'" >&2
-    exit 1;
     ;;
 esac
 
-run-parts "${BASEDIR}"/hooks/"${operation}".d/ "$@"
+run_parts "${BASEDIR}"/hooks/"${operation}".d/ "$@"
+~                                                    
