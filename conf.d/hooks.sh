4e23bac6b91f236a4e9468e661cd48b7cdaf9d8b
Nick <nick@kousu.ca>
2021-04-19 23:37:20 -0400

    Update hooks.sh

--- conf.d/hooks.sh
+++ conf.d/hooks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "${HOOK:-}" ]; then 
+if [ -n "${HOOK:-}" ]; then 
   echo "hooks.sh: \$HOOK already defined: ${HOOK}. Please uninstall the conflicting dehydrated plugin to use dehydrated-hooks." >&2
   exit 1
 fi
